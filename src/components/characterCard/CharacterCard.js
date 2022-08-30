5778f7209adaecd11bb4e011514dcf71f79825a4
JSSatorres <juansataz.dev@gmail.com>
2022-08-30 12:16:21 +0200

    cleaning character card

--- src/components/characterCard/CharacterCard.js
+++ src/components/characterCard/CharacterCard.js
@@ -5,7 +5,6 @@ function CharacterCard({ character }) {
     <section key={character.id} className="col-6 px-2">
       <div className="container py-2">
         <div className="row d-flex ">
-          {/* <div className="col col-md-9 col-lg-7 col-xl-5"> */}
           <div className="card">
             <div className="card-body p-4">
               <div className="d-flex text-black col-4">
@@ -33,25 +32,10 @@ function CharacterCard({ character }) {
                       <p className="mb-0">{character.status}</p>
                     </div>
                   </div>
-                  {/*   <div className="d-flex pt-1">
-                    <button
-                      type="button"
-                      className="btn btn-outline-primary me-1 flex-grow-1"
-                    >
-                      Chat
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-primary flex-grow-1"
-                    >
-                      Follow
-                    </button> 
-                  </div>*/}
                 </div>
               </div>
             </div>
           </div>
-          {/* </div> */}
         </div>
       </div>
     </section>
